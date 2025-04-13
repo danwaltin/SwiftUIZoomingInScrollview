e26e3ba1a3737fa8e921b5108a8771b087c0da07
Dan Waltin <dan@rakel.local>
2025-04-13 16:59:08 +0200

    Adding .gitignore and a project

---
+++ SwiftUIZoomingInScrollview/SwiftUIZoomingInScrollviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIZoomingInScrollviewApp.swift
+//  SwiftUIZoomingInScrollview
+//
+//  Created by Dan Waltin on 2025-04-13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIZoomingInScrollviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
